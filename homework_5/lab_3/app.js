5e82ae3296df376c6241cddc25523cd3f876a1dd
Zinaida <zinaida.ivanova.1988@mail.ru>
2020-07-31 18:54:54 +0300

    lab 5

---
+++ homework_5/lab_3/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser')
+
+app.listen(8080);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.route('/reg').post((req, res) => {
+      console.log('Параметры POST запроса: ' + JSON.stringify(req.body));
+      res.send(JSON.stringify(req.body));
+});
+
+
